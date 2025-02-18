6a4bb86659db08c79b5ca0de617d4170f18f2c1a
adambor <adamborteam@gmail.com>
2025-02-18 13:13:41 +0100

    Add additional types to export

--- dist/chains/ChainData.d.ts
+++ dist/chains/ChainData.d.ts
@@ -1,5 +1,9 @@
 import { ChainType } from "./ChainType";
 import { BtcRelay } from "../btcrelay/BtcRelay";
+import { BitcoinRpc } from "../btcrelay/rpc/BitcoinRpc";
+import { BitcoinNetwork } from "../btc/BitcoinNetwork";
+import { IStorageManager } from "../storage/IStorageManager";
+import { StorageObject } from "../storage/StorageObject";
 export type BaseTokenType<T extends string = string> = {
     [ticker in T]: {
         address: string;
@@ -14,4 +18,12 @@ export type ChainData<T extends ChainType> = {
     chainEvents: T["Events"];
     swapDataConstructor: new (data: any) => T["Data"];
     storagePrefix?: string;
+};
+export type ChainInitializerFn<O, C extends ChainType> = (options: O, bitcoinRelay: BitcoinRpc<any>, network: BitcoinNetwork, storageCtor: <T extends StorageObject>(name: string) => IStorageManager<T>) => ChainData<C>;
+export type ChainInitializer<O, C extends ChainType, T extends BaseTokenType> = {
+    chainId: C["ChainId"];
+    chainType: C;
+    initializer: ChainInitializerFn<O, C>;
+    tokens: T;
+    options: O;
 };
--- src/chains/ChainData.ts
+++ src/chains/ChainData.ts
@@ -2,6 +2,8 @@ import {ChainType} from "./ChainType";
 import {BtcRelay} from "../btcrelay/BtcRelay";
 import {BitcoinRpc} from "../btcrelay/rpc/BitcoinRpc";
 import {BitcoinNetwork} from "../btc/BitcoinNetwork";
+import {IStorageManager} from "../storage/IStorageManager";
+import {StorageObject} from "../storage/StorageObject";
 
 export type BaseTokenType<T extends string = string> = {
     [ticker in T]: {
@@ -20,10 +22,17 @@ export type ChainData<T extends ChainType> = {
     storagePrefix?: string
 };
 
-type ChainInitializer<O, C extends ChainType, T extends BaseTokenType> = {
+export type ChainInitializerFn<O, C extends ChainType> = (
+    options: O,
+    bitcoinRelay: BitcoinRpc<any>,
+    network: BitcoinNetwork,
+    storageCtor: <T extends StorageObject>(name: string) => IStorageManager<T>
+) => ChainData<C>;
+
+export type ChainInitializer<O, C extends ChainType, T extends BaseTokenType> = {
     chainId: C["ChainId"],
     chainType: C,
-    initializer: (options: O, bitcoinRelay: BitcoinRpc<any>, network: BitcoinNetwork) => ChainData<C>,
+    initializer: ChainInitializerFn<O, C>,
     tokens: T,
     options: O
 }
