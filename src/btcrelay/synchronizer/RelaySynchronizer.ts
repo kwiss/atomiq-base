7ff02964f17844f9ae95c129d1d1ccd55d061ecb
adambor <adamborteam@gmail.com>
2025-03-30 21:00:00 +0200

    Change signature of the synchronization function

--- src/btcrelay/synchronizer/RelaySynchronizer.ts
+++ src/btcrelay/synchronizer/RelaySynchronizer.ts
@@ -5,12 +5,13 @@ import {BtcBlock} from "../types/BtcBlock";
 export interface RelaySynchronizer<V extends BtcStoredHeader<any>, T, B extends BtcBlock> {
 
     syncToLatestTxs(signer: string, feeRate?: string): Promise<{
-        txs: T[]
-        targetCommitedHeader: V,
-        computedHeaderMap: {[blockheight: number]: V},
-        blockHeaderMap: {[blockheight: number]: B},
-        btcRelayTipBlockHash: string,
-        latestBlockHeader: B,
+        txs: T[], //Transactions required to synchronize the btc relay
+        targetCommitedHeader: V, //Latest committed header after synchronization
+        latestBlockHeader: B, //Latest block header after synchronization
+        computedHeaderMap: {[blockheight: number]: V}, //Mapping of synchronized committed headers
+        blockHeaderMap: {[blockheight: number]: B}, //Mapping of synchronized block headers
+        btcRelayTipCommitedHeader: V, //Tip committed header of the btc relay before synchronization
+        btcRelayTipBlockHeader: B, //Tip block header of the btc relay before synchronization
         startForkId?: number
     }>;
 
